e5cb1d25c4728129fb7719e33c47a6135faeb5f5
Andrew <andy@andycondon.com>
2017-09-27 11:51:13 +1300

    10 completed

--- giftgiver/src/components/App.test.js
+++ giftgiver/src/components/App.test.js
@@ -14,8 +14,12 @@ it('initialises the `state` with an empty list of gifts', () => {
 
 it('adds a new gift to `state` when clicking the `add gift` button', () => {
   app.find('.btn-add').simulate('click')
+
   expect(app.state().gifts).toEqual([{ id: 1 }])
-  
+})
+
+it('adds a new gift to the rendered list when clicking the `add gift` button', () => {
   app.find('.btn-add').simulate('click')
-  expect(app.state().gifts).toEqual([{ id: 1 }, { id: 2 }])
+
+  expect(app.find('.gift-list').children().length).toEqual(2)
 })
