6431044ee2de5ef309bffea079b4c81ecaba429d
andrew-rayco <andy@andycondon.com>
2017-09-28 04:44:46 +1300

    resolve issues with requestAnimationFrame errors and react 16 adapter

---
+++ lootcheck/src/components/Wallet.test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import Wallet from './Wallet'
+
+
+describe('Wallet', () => {
+  const wallet = shallow(<Wallet />)
+
+  it('renders properly', () => {
+    expect(wallet).toMatchSnapshot()
+  })
+})
