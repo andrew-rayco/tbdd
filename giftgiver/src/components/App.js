b555d9f896c725f5682cf13ee7273c3494c35f62
Andrew <andy@andycondon.com>
2017-09-27 13:06:43 +1300

    16 completed

--- giftgiver/src/components/App.js
+++ giftgiver/src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { button } from 'react-bootstrap'
 import Gift from './Gift'
+import { max_number } from '../helper'
 
 class App extends Component {
   constructor() {
@@ -12,10 +13,8 @@ class App extends Component {
 
   addGift = () => {
     const { gifts } = this.state
-    const ids = this.state.gifts.map(gift => gift.id)
-    const max_id = ids.length > 0 ? Math.max(...ids) : 0
 
-    gifts.push({ id: max_id + 1 })
+    gifts.push({ id: max_number(this.state.gifts.map(gift => gift.id)) + 1 })
     this.setState({ gifts })
   }
 
@@ -35,7 +34,7 @@ class App extends Component {
               <Gift
                 key={gift.id}
                 gift={gift}
-                removeGift={this.removeGift} 
+                removeGift={this.removeGift}
               />
             )
           }) }
