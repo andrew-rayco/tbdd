72514d9c7ab7ab745d26891680c8bb9da9dbb4ce
Andrew <andy@andycondon.com>
2017-09-30 13:01:34 +1300

    31 completed

--- lootcheck/src/components/Wallet.test.js
+++ lootcheck/src/components/Wallet.test.js
@@ -4,7 +4,8 @@ import { Wallet } from './Wallet'
 
 describe('Wallet', () => {
   const mockDeposit = jest.fn()
-  const props = { balance: 20, deposit: mockDeposit }
+  const mockWithdraw = jest.fn()
+  const props = { balance: 20, deposit: mockDeposit, withdraw: mockWithdraw }
   const wallet = shallow(<Wallet {...props} />)
 
   it('renders properly', () => {
@@ -31,14 +32,25 @@ describe('Wallet', () => {
       expect(wallet.state().balance).toEqual(parseInt(userBalance, 10))
     })
 
-    describe('and the user wantes to make a deposit', () => {
+    describe('and the user wants to make a deposit', () => {
       beforeEach(() => {
         wallet.find('.btn-deposit')
           .simulate('click')
       })
 
-      it('dispatches the `deposit()` it receives from props with local balance', () => {
+      it('dispatches the `deposit()` it receives from props with the local balance', () => {
         expect(mockDeposit).toHaveBeenCalledWith(parseInt(userBalance, 10))
+      })
+    })
+
+    describe('and the user wants to make a withdrawal', () => {
+      beforeEach(() => {
+        wallet.find('.btn-withdraw')
+          .simulate('click')
+      })
+
+      it('dispatches the `withdraw()` it receives from props with the local balance', () => {
+        expect(mockWithdraw).toHaveBeenCalledWith(parseInt(userBalance, 10))
       })
     })
   })
