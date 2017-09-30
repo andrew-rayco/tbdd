431fbe8e1831649cc36e6b3d3e84b361f4d7d6eb
Andrew <andy@andycondon.com>
2017-09-30 14:46:15 +1300

    40 completed

---
+++ lootcheck/src/components/Loot.test.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { mount, shallow } from 'enzyme'
+import { Loot } from './Loot'
+
+describe('Loot', () => {
+  const mockFetchBitcoin = jest.fn()
+  let props = { balance: 10, bitcoin: {}, fetchBitcoin: mockFetchBitcoin }
+  let loot = shallow(<Loot {...props} />)
+
+  it('renders properly', () => {
+    expect(loot).toMatchSnapshot()
+  })
+
+  describe('when mounted', () => {
+    beforeEach(() => {
+      props.fetchBitcoin = mockFetchBitcoin
+
+      loot = mount(<Loot {...props} />)
+    })
+
+    it('dispatches the `fetchBitcoin()` method it receives from props', () => {
+      expect(mockFetchBitcoin).toHaveBeenCalled()
+    })
+  })
+})
